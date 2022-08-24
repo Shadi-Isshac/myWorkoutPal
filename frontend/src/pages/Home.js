67a2f44dcbb17c8fac326e657ef5803f80f8d9cb
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-08-24 17:10:34 -0500

    context and hook created. gitignore moved to root

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,33 +1,32 @@
-import { useEffect, useState } from "react"
-import WorkoutDetails from '../components/WorkoutDetails'
+import { useEffect } from "react"
+import {UseWorkoutsContext} from "../hooks/UseWorkoutsContext"
+import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
+  const { workouts, dispatch } = UseWorkoutsContext()
 
-    const [workouts, setWorkouts] = useState(null)
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
 
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
-            const json = await response.json()
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
 
-            if (response.ok){
-                setWorkouts(json)
-            }
-        }
-
-        fetchWorkouts()
-    }, [])
-
+    fetchWorkouts()
+  }, [dispatch])
 
   return (
     <div className="home">
-        <div className="workouts">
-            {workouts && workouts.map((workout) => (
-               <WorkoutDetails key={workout._id} workout={workout}/>
-            ))}
-        </div>
-        <WorkoutForm />
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+      <WorkoutForm />
     </div>
   )
 }
