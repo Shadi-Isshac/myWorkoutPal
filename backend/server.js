61c8a596aa886c6d924ecdf0098c8a969c4fd26c
Shadi Isshac <shadi@Shadis-MBP.attlocal.net>
2022-08-23 21:39:14 -0500

    server is listening on port 4000

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
 const express = require('express')
 
+// created express app
 const app = express()
+
+// listen for requests
+
+app.listen(4000, () => {
+    console.log('server is listening!')
+})
+
