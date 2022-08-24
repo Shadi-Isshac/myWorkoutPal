a2423fcb2eb3ff09170f2ed2569ce14a060afc4d
Shadi Isshac <shadi@Shadis-MBP.attlocal.net>
2022-08-23 22:00:08 -0500

    req method and path displaying in terminal through middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const express = require('express')
 // created express app
 const app = express()
 
+// middleware
+app.use((req,res,next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
 app.get('/', (req,res) => {
     res.json({messg:'Welcome to the app'})
 })
@@ -14,4 +21,6 @@ app.get('/', (req,res) => {
 app.listen(process.env.PORT, () => {
     console.log('server is listening')
 })
+
+
 
