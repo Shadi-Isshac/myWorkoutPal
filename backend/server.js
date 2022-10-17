ff92ab1c60e02c5c7a01cc1d644efbc39570da0f
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-10-17 15:47:41 -0500

    .

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,6 @@ app.use((req,res,next) => {
 app.use('/api/workouts', workoutRoutes)
 
 // connection to db
-
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     app.listen(process.env.PORT, () => {
